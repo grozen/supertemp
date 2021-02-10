14dd21b671f80ba77673ff76e68529d94c7fb8ec
grozen <grozen>
2021-02-10 16:46:25 +0000

    Initial notebook push

---
+++ tiny.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md I am the smallest notebook in town.
+# MAGIC 1. Herp
+# MAGIC 2. Aderp
+# MAGIC 3. Lerp
+# MAGIC 
+# MAGIC Oh check it out, a code snippet!
+# MAGIC 
+# MAGIC ```
+# MAGIC Snip snip sinp!
+# MAGIC ```
+
+# COMMAND ----------
+
+x = 2
+print("moo"*x)
